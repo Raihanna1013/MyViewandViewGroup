f57b0124cdbd7a26d3951ec13b932d344be1a3c5
hamid <55974792+Raihanna1013@users.noreply.github.com>
2023-03-08 10:43:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myviewandviewgroup/MainActivityConstrain.kt
@@ -0,0 +1,11 @@
+package com.example.myviewandviewgroup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityConstrain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_constrain)
+    }
+}
